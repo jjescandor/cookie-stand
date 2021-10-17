add7e143e5bd2a2cffeedc49943026850e7d58d4
JJ Escandor <jj.p.escandor@gmail.com>
2021-10-17 16:57:23 -0400

    optimized the code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 "use strict";
 //Arrays used in the app
-let times = ['Store Location', '0600 AM', '0700 AM', '0800 AM', '0900 AM', '1000 AM', '1100 AM',
+let columnsArray = ['Store Location', '0600 AM', '0700 AM', '0800 AM', '0900 AM', '1000 AM', '1100 AM',
     '1200 PM', '0100 PM', '0200 PM', '0300 PM', '0400 PM', '0500 PM', '0600 PM', '0700 PM', 'Daily Location Total'];
-let cookieStoresTotalSalesArray = [];
+let cookieSalesPerStoreArray = [];
 let totalSalesPerHourArray = [];
 let storeInstanceArray = [];
-//Grab the anchor table elements
+//HTML tags that are used as anchors for DOM manipulation 
 let cookieStoreHeader = document.getElementById('cookieStoreHeader')
 let cookieStoreSales = document.getElementById('cookieStoreSales');
 let salesPerHourFooter = document.getElementById('salesPerHourFooter')
@@ -26,17 +26,17 @@ function CookieStore(location, min, max, avg) {
         let storeLocation = document.createElement('tr');
         storeLocation.textContent = this.location
         cookieStoreSales.appendChild(storeLocation);
-        let sum = 0;
-        for (let i = 0; i < times.length - 2; i++) {
+        let sumPerStore = 0;
+        for (let i = 0; i < columnsArray.length - 2; i++) {
             this.cookiesSoldEachHourArray.push(this.getCookieSalesPerHour());
-            sum += this.cookiesSoldEachHourArray[i];
+            sumPerStore += this.cookiesSoldEachHourArray[i];
             let cookieSalesPerHour = document.createElement('td');
             cookieSalesPerHour.textContent = this.cookiesSoldEachHourArray[i];
             storeLocation.appendChild(cookieSalesPerHour);
         }
-        cookieStoresTotalSalesArray.push(sum);
+        cookieSalesPerStoreArray.push(sumPerStore);
         let totalSalesPerStore = document.createElement('td');
-        totalSalesPerStore.textContent = sum;
+        totalSalesPerStore.textContent = sumPerStore;
         storeLocation.appendChild(totalSalesPerStore);
     };
     storeInstanceArray.push(this);
@@ -49,10 +49,10 @@ new CookieStore('Paris', 20, 38, 2.3);
 new CookieStore('Lima', 2, 16, 4.6);
 //Function that renders the table header
 function renderCookieStoreTableHeader() {
-    for (let i = 0; i < times.length; i++) {
-        let th = document.createElement('th');
-        th.textContent = times[i];
-        cookieStoreHeader.appendChild(th);
+    for (let i = 0; i < columnsArray.length; i++) {
+        let tableHeader = document.createElement('th');
+        tableHeader.textContent = columnsArray[i];
+        cookieStoreHeader.appendChild(tableHeader);
     }
     for (let i = 0; i < storeInstanceArray.length; i++) {
         storeInstanceArray[i].renderTableTbody();
@@ -65,17 +65,18 @@ function renderTableFooter() {
     totalSalesHourTitle.textContent = 'Total';
     salesPerHourFooter.appendChild(totalSalesHourTitle);
     let totalOfTotal = 0;
-    for (let i = 0; i < times.length - 2; i++) {
+    //Finds the total number of cookies sold per hour in all stores
+    for (let i = 0; i < columnsArray.length - 2; i++) {
         let totalSalesPerHourinAllstores = 0;
         let totalSalesPerHour = document.createElement('td');
         for (let j = 0; j < storeInstanceArray.length; j++) {
             totalSalesPerHourinAllstores += storeInstanceArray[j].cookiesSoldEachHourArray[i];
-            console.log(totalSalesPerHourinAllstores);
+            totalSalesPerHourinAllstores;
         }
         totalSalesPerHourArray.push(totalSalesPerHourinAllstores);
-        totalOfTotal += totalSalesPerHourinAllstores;
         totalSalesPerHour.textContent = totalSalesPerHourArray[i];
         totalSalesHourTitle.appendChild(totalSalesPerHour);
+        totalOfTotal += totalSalesPerHourinAllstores;
     }
     let totalSalesInAllStores = document.createElement('td');
     totalSalesInAllStores.textContent = totalOfTotal;
@@ -84,5 +85,6 @@ function renderTableFooter() {
 //Invokes the entire program
 renderTableFooter();
 
+console.log(cookieSalesPerStoreArray)
 
 
