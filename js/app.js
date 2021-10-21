8ddc60c04631c97cd87d84df966d14a8efdbe435
JJ Escandor <jj.p.escandor@gmail.com>
2021-10-21 19:16:12 -0400

    Styled Pat's Salmon Cookies website

--- js/app.js
+++ js/app.js
@@ -1,99 +1,74 @@
 "use strict";
-//Arrays used in the app
-let columnsArray = ['Store Location', '0600 AM', '0700 AM', '0800 AM', '0900 AM', '1000 AM', '1100 AM',
-    '1200 PM', '0100 PM', '0200 PM', '0300 PM', '0400 PM', '0500 PM', '0600 PM', '0700 PM', 'Daily Location Total'];
-let cookieSalesPerStoreArray = [];
-let totalSalesPerHourArray = [];
-let storeInstanceArray = [];
 
-//Tags in sales.html that are used as anchors for DOM manipulation 
-let cookieStoreHeader = document.querySelector('thead tr');
-let cookieStoreSales = document.querySelector('tbody');
-let salesPerHourFooter = document.querySelector('tfoot');
+//landing page JS Codee
 
-//Object constructor function
-function CookieStore(location, min, max, avg) {
-    this.location = location;
-    this.min = min;
-    this.max = max;
-    this.avg = avg;
-    this.cookiesSoldEachHourArray = [];
-    //Generates random cookie sales per hour
-    this.getCookieSalesPerHour = function () {
-        let cookiesPerHour = Math.ceil(Math.floor(Math.random() * (this.max - this.min + 1)
-            + this.min) * this.avg);
-        return cookiesPerHour;
-    };
-    //Renders tbody section of the Table which is invoked in renderCookieStoreTableHeader()
-    this.renderTableTbody = function () {
-        let trStoreLocation = document.createElement('tr');
-        cookieStoreSales.appendChild(trStoreLocation);
-        let storeLocation = document.createElement('td');
-        storeLocation.textContent = this.location;
-        trStoreLocation.appendChild(storeLocation);
-        let sumPerStore = 0;
-        for (let i = 0; i < columnsArray.length - 2; i++) {
-            this.cookiesSoldEachHourArray.push(this.getCookieSalesPerHour());
-            sumPerStore += this.cookiesSoldEachHourArray[i];
-            let cookieSalesPerHour = document.createElement('td');
-            cookieSalesPerHour.textContent = this.cookiesSoldEachHourArray[i];
-            trStoreLocation.appendChild(cookieSalesPerHour);
+let arrowButtons = document.getElementsByTagName('span');
+let banner = document.getElementsByClassName('banner');
+let blockQuote = document.querySelector('blockquote');
+let index = 0;
+
+arrowButtons[1].onclick = () => {
+    if (index == banner.length - 1) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+    showbanner();
+    pas();
+}
+
+arrowButtons[0].onclick = () => {
+    if (index == 0) {
+        index = banner.length - 1;
+    }
+    else {
+        index--;
+    }
+    showbanner();
+    leftslide();
+    pas();
+}
+
+function showbanner() {
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].classList.remove('active');
+    }
+    banner[index].classList.add('active');
+}
+
+function leftslide() {
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].classList.remove('left');
+    }
+    banner[index].classList.add('left')
+}
+
+//create dots 
+
+function dots() {
+    for (let i = 0; i < banner.length; i++) {
+        let p = document.createElement('p');
+        p.setAttribute('onlick', 'indicate(this)');
+        p.id = i;
+        if (i == 0) {
+            p.className = 'actual';
         }
-        cookieSalesPerStoreArray.push(sumPerStore);
-        let totalSalesPerStore = document.createElement('td');
-        totalSalesPerStore.textContent = sumPerStore;
-        trStoreLocation.appendChild(totalSalesPerStore);
-    };
-    storeInstanceArray.push(this);
-}
-
-//Instances of the CookieStore object constructor function
-new CookieStore('Seattle', 23, 65, 6.3);
-new CookieStore('Tokyo', 3, 24, 1.2);
-new CookieStore('Dubai', 11, 38, 3.7);
-new CookieStore('Paris', 20, 38, 2.3);
-new CookieStore('Lima', 2, 16, 4.6);
-
-//Renders the header section of the table
-function renderCookieStoreTableHeader() {
-    for (let i = 0; i < columnsArray.length; i++) {
-        let tableHeader = document.createElement('td');
-        tableHeader.textContent = columnsArray[i];
-        cookieStoreHeader.appendChild(tableHeader);
-    }
-    //Invokes renderTableTbody() on all instances of CookieStore object
-    for (let i = 0; i < storeInstanceArray.length; i++) {
-        storeInstanceArray[i].renderTableTbody();
+        blockQuote.appendChild(p);
     }
 }
+dots();
 
-//Renders footer section of the table
-function renderTableFooter() {
-    //Invokes renderCookieStoreTableHeader()
-    renderCookieStoreTableHeader();
-    let trTotal = document.createElement('tr');
-    salesPerHourFooter.appendChild(trTotal);
-    let totalSalesHourTitle = document.createElement('td');
-    totalSalesHourTitle.textContent = 'Total';
-    trTotal.appendChild(totalSalesHourTitle);
-    let totalOfTotal = 0;
-    //Finds the total number of cookies sold per hour in all stores
-    for (let i = 0; i < columnsArray.length - 2; i++) {
-        let totalSalesPerHourinAllstores = 0;
-        let totalSalesPerHour = document.createElement('th');
-        for (let j = 0; j < storeInstanceArray.length; j++) {
-            totalSalesPerHourinAllstores += storeInstanceArray[j].cookiesSoldEachHourArray[i];
-            totalSalesPerHourinAllstores;
-        }
-        totalSalesPerHourArray.push(totalSalesPerHourinAllstores);
-        totalSalesPerHour.textContent = totalSalesPerHourArray[i];
-        trTotal.appendChild(totalSalesPerHour);
-        //Stores the total number of sales from 6am - 7pm in all stores in a variable
-        totalOfTotal += totalSalesPerHourinAllstores;
+//turn dots into nav
+
+const pas = () => {
+    for (let i = 0; i < blockQuote.children.length; i++) {
+        blockQuote.children[i].classList.remove('actual');
     }
-    let totalSalesInAllStores = document.createElement('td');
-    totalSalesInAllStores.textContent = totalOfTotal;
-    trTotal.appendChild(totalSalesInAllStores);
+    blockQuote.children[index].classList.add('actual');
 }
-//Invokes renderTableFooter(), intitializes the entire program
-renderTableFooter();
+
+
+
+
+
